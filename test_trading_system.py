985c53c1dba1330a328f00bb00a24c7988c5d089
dykim <dykim4225@gmail.com>
2025-08-04 10:32:37 +0900

    [feature] Add basic unit tests
    Signed-off-by: dykim <dykim4225@gmail.com>

--- test_trading_system.py
+++ test_trading_system.py
@@ -1,4 +1,52 @@
 import pytest
 
-def test_sample():
-    assert 1 == 1
+from auto_trading_system import AutoTradingSystem
+
+@pytest.fixture
+def trading_system():
+    trading_system = AutoTradingSystem()
+    trading_system.select_stock_broker("Kiwer")
+    trading_system.login("test", "1234")
+
+def test_basic_function_select_broker():
+    trading_system = AutoTradingSystem()
+
+    trading_system.select_stock_broker("Kiwer")
+    assert trading_system.get_stock_broker() == "Kiwer"
+
+    trading_system.select_stock_broker("Nemo")
+    assert trading_system.get_stock_broker() == "Nemo"
+
+
+def test_basic_function_login_kiwer(capsys):
+    trading_system = AutoTradingSystem()
+
+    trading_system.select_stock_broker("Kiwer")
+    trading_system.login("test", "1234")
+
+    captured = capsys.readouterr()
+    assert "test login success" in captured.out
+
+def test_basic_function_login_nemo(capsys):
+    trading_system = AutoTradingSystem()
+
+    trading_system.select_stock_broker("Nemo")
+    trading_system.login("test", "1234")
+
+    captured = capsys.readouterr()
+    assert "test login success" in captured.out
+
+def test_basic_function_buy(capsys, trading_system):
+    trading_system.buy("1234", 10000, 10)
+    captured = capsys.readouterr()
+    assert "1234 : Buy stock ( 10000 * 10 )" in captured.out
+
+def test_basic_function_sell(capsys, trading_system):
+    trading_system.buy("1234", 10000, 10)
+    trading_system.sell("1234", 10000, 10)
+    captured = capsys.readouterr()
+    assert "1234 : Sell stock ( 10000 * 10 )" in captured.out
+
+def test_basic_function_current_price(capsys, trading_system):
+    price = trading_system.current_price("1234")
+    assert (price >=0 and price < 10000)
