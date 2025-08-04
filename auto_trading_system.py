5a25ab79bbff04c480778fbbfe73c27f482a66ca
Janghee.Lee <lee.janghee@gmail.com>
2025-08-04 12:23:50 +0900

    add basic feature - passed for tc1

--- auto_trading_system.py
+++ auto_trading_system.py
@@ -1,2 +1,21 @@
 class AutoTradingSystem:
-    pass
+    def __init__(self):
+        self._broker_name = ""
+
+    def select_stock_broker(self, name):
+        self._broker_name = name
+
+    def login(self, name, password):
+        print("test login success")
+
+    def get_stock_broker(self):
+        return self._broker_name
+
+    def buy(self, code: str, price, quantity):
+        print(f'{code} : Buy stock ( {price} * {quantity} )')
+
+    def sell(self, code: str, price, quantity):
+        print(f'{code} : Sell stock ( {price} * {quantity} )')
+
+    def current_price(self, code: str):
+        return 100
