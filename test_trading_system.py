ddc5f624261004c9f8ed724a4898162c3192cb28
dykim <dykim4225@gmail.com>
2025-08-04 15:19:03 +0900

    [test] add test for driver usage
    Signed-off-by: dykim <dykim4225@gmail.com>

--- test_trading_system.py
+++ test_trading_system.py
@@ -39,18 +39,20 @@ def test_basic_function_login_kiwer(capsys, trading_system, mocker: MockerFixtur
     driver.login.side_effect = mocker_login
     trading_system.select_stock_broker("Kiwer", driver)
     trading_system.login("test", "1234")
+
     captured = capsys.readouterr()
     assert "test login success" in captured.out
-
+    driver.login.assert_called()
 
 def test_basic_function_login_nemo(capsys, trading_system, mocker: MockerFixture):
     driver: NemoDriver = mocker.Mock()
     driver.login.side_effect = mocker_login
     trading_system.select_stock_broker("Nemo", driver)
     trading_system.login("test", "1234")
+
     captured = capsys.readouterr()
     assert "test login success" in captured.out
-
+    driver.login.assert_called()
 
 def test_basic_function_buy(capsys, trading_system, mocker: MockerFixture):
     driver: KiwerDriver = mocker.Mock()
@@ -58,9 +60,10 @@ def test_basic_function_buy(capsys, trading_system, mocker: MockerFixture):
 
     trading_system.select_stock_broker("Kiwer", driver)
     trading_system.buy("1234", 10000, 10)
+
     captured = capsys.readouterr()
     assert "1234 : Buy stock ( 10000 * 10 )" in captured.out
-
+    driver.buy.assert_called()
 
 def test_basic_function_sell(capsys, trading_system, mocker: MockerFixture):
     driver: KiwerDriver = mocker.Mock()
@@ -69,16 +72,20 @@ def test_basic_function_sell(capsys, trading_system, mocker: MockerFixture):
     trading_system.select_stock_broker("Kiwer", driver)
 
     trading_system.sell("1234", 10000, 10)
+
     captured = capsys.readouterr()
     assert "1234 : Sell stock ( 10000 * 10 )" in captured.out
+    driver.sell.assert_called()
 
 
 def test_basic_function_current_price(capsys, trading_system, mocker: MockerFixture):
     driver: KiwerDriver = mocker.Mock()
     driver.get_price.return_value = 100
     trading_system.select_stock_broker("Kiwer", driver)
+
     price = trading_system.get_price("1234")
     assert price == 100
+    driver.get_price.assert_called()
 
 
 def test_buy_called_on_three_price_increase(mocker):
